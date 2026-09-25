cd11cba4e13c87c9a2edceeec0228145b06e68b2
guientregas <colegjo@gmail.com>
2026-09-25 20:16:18 -0300

    V25: carregar correções finais de interface e localização WhatsApp

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
 window.JT_CONFIG={API_BASE:"",SESSION_ID:"default",APP_VERSION:"V25"};
-(function(){var s=document.createElement('script');s.src='./ui-v25.js?v=20260925';s.defer=false;document.head.appendChild(s)})();
+(function(){var a=document.createElement('script');a.src='./ui-v25.js?v=20260925';document.head.appendChild(a);var b=document.createElement('script');b.src='./fix-v25.js?v=20260925';document.head.appendChild(b)})();
