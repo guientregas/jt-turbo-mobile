7e62691710cd9eddcf859f3027239a1b7a7ed2a3
guientregas <colegjo@gmail.com>
2026-09-25 20:16:07 -0300

    V25: carregar interface simplificada e cadastro de localização do WhatsApp

--- v23/config.js
+++ v23/config.js
@@ -1,1 +1,2 @@
-window.JT_CONFIG={API_BASE:"",SESSION_ID:"default",APP_VERSION:"V24.1"};
+window.JT_CONFIG={API_BASE:"",SESSION_ID:"default",APP_VERSION:"V25"};
+(function(){var s=document.createElement('script');s.src='./ui-v25.js?v=20260925';s.defer=false;document.head.appendChild(s)})();
