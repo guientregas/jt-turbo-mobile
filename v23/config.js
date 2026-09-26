70f3db5c241a53910c96bbc03c359dfd7519df6d
guientregas <colegjo@gmail.com>
2026-09-26 13:48:34 -0300

    Legacy redirecionar para V34

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
-window.JT_CONFIG={APP_VERSION:'V32-FINAL',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
-(function(){location.replace('../v32/index.html?v=20260926-v32')})();
+window.JT_CONFIG={APP_VERSION:'V34-HEAVY-AUDIT',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
+(function(){location.replace('../v33/index.html?v=20260926-v34')})();
