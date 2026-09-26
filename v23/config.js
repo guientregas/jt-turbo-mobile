d3d02f08637352e28664728cd07ef83ad9bf3bca
guientregas <colegjo@gmail.com>
2026-09-26 13:50:57 -0300

    Direcionar entrada para V35

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
 window.JT_CONFIG={APP_VERSION:'V35-HEAVY-AUDIT',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
-(function(){location.replace('../v34/index.html?v=20260926-v35')})();
+(function(){location.replace('../v35/index.html?v=20260926-v35')})();
