340b941e3886d6b39dab9fb6b240adde678bb494
guientregas <colegjo@gmail.com>
2026-09-26 13:11:01 -0300

    V29 apontar app e limpar cache antigo

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
-window.JT_CONFIG={APP_VERSION:'V28-STABLE',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
-(function(){var target='./app-final.html?v=20260926-v28';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
+window.JT_CONFIG={APP_VERSION:'V29-NAV-FIX',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
+(function(){var target='./app-final.html?v=20260926-v29';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
