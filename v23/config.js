939ba55ad008df2d88657cf7515acec6cd5d7221
guientregas <colegjo@gmail.com>
2026-09-26 01:58:56 -0300

    V27: atualizar apontamento do app

--- v23/config.js
+++ v23/config.js
@@ -1,7 +1,2 @@
-window.JT_CONFIG={APP_VERSION:'V26-STABLE',APP_NAME:'J&T e iMile'};
-(function(){
-  var target='./app-final.html?v=20260926-v26';
-  if(location.pathname.indexOf('/v23/')!==-1 && !location.pathname.endsWith('/app-final.html')){
-    location.replace(target);
-  }
-})();
+window.JT_CONFIG={APP_VERSION:'V27-STABLE',APP_NAME:'J&T e iMile'};
+(function(){var target='./app-final.html?v=20260926-v27';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
