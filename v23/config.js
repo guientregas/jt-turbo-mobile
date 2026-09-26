b5324345e8debbe0190a57d7c22f21fbc4206344
guientregas <colegjo@gmail.com>
2026-09-26 13:40:04 -0300

    Redirecionar para V32 final

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
-window.JT_CONFIG={APP_VERSION:'V31-ROUTE-GUARD',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
-(function(){var target='./app-final.html?v=20260926-v31';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
+window.JT_CONFIG={APP_VERSION:'V32-FINAL',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
+(function(){location.replace('../v32/index.html?v=20260926-v32')})();
