92812127ed54b635b16bedb5ed26faff52bb53cb
guientregas <colegjo@gmail.com>
2026-09-26 03:55:18 -0300

    V28: configuração estável e provedor de geocodificação

--- v23/config.js
+++ v23/config.js
@@ -1,2 +1,2 @@
-window.JT_CONFIG={APP_VERSION:'V27-STABLE',APP_NAME:'J&T e iMile'};
-(function(){var target='./app-final.html?v=20260926-v27';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
+window.JT_CONFIG={APP_VERSION:'V28-STABLE',APP_NAME:'J&T e iMile',GEOCODER_URL:''};
+(function(){var target='./app-final.html?v=20260926-v28';if(location.pathname.indexOf('/v23/')!==-1&&!location.pathname.endsWith('/app-final.html'))location.replace(target)})();
